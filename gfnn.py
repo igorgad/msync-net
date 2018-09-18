132f5255a51e430ab0c4f7eadec3e92da452d20f
igorgad <igorgad@gmail.com>
2018-09-18 14:23:35 +0200

    multi osc

--- gfnn.py
+++ gfnn.py
@@ -1,4 +1,3 @@
-
 # Author - Igor Pereira
 # UFRN - ISASI
 
@@ -7,27 +6,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def single_oscillator(alpha, omega, beta1, beta2, delta1, delta2, eps, k, dt, x_in):
-    z = tf.Variable(tf.complex(0.01, 0.0))
-    x = tf.complex(x_in, 0.0)
+#def single_oscillator(alpha, omega, beta1, beta2, delta1, delta2, eps, k, dt, x_in):
+#    return step
 
-    a = tf.complex(alpha, omega)
-    b = tf.complex(beta1, delta1)
-    d = tf.complex(beta2, delta2)
-    z2 = tf.complex(tf.pow(tf.abs(z), 2), 0.0)
-    z4 = tf.complex(tf.pow(tf.abs(z), 4), 0.0)
-    z_ = tf.conj(z)
-    passive = x * tf.reciprocal(1.0 - tf.sqrt(eps) * x)
-    active = tf.reciprocal(1.0 - tf.sqrt(eps) * z_)
 
-    z_step = dt * (tf.multiply(z, a + b * z2 + d * eps * z4 / (1 - eps * z2)) + k * passive * active)
-    step = z.assign(z + z_step)
-    return step
 
 
 dt = 0.01667  # Fs = 60Hz
-alpha = -0.5
-omega = 2.0*np.pi
+t = np.arange(0, 20, dt)
+sin = 0.25 * np.sin(t*2*np.pi)
+#sin = np.concatenate([0.25 * np.sin(t[:t.size//2]*1*np.pi),  np.zeros(t.size//2)])
+
+omega = np.arange(0, 2.0*np.pi, np.pi/10, np.float32)
+alpha = -0.5 * np.ones_like(omega, np.float32)
 beta1 = -1.0
 beta2 = 0.0
 delta1 = -1.0
@@ -35,21 +26,42 @@ delta2 = 0.0
 eps = np.complex64(1.0 + 0j)
 k = np.complex64(1.0 + 0j)
 
+z_init_r = 0.01 * np.ones_like(alpha, np.float32)
+z_init_i = 0.00 * np.ones_like(alpha, np.float32)
+
 
 x_in = tf.placeholder(tf.float32)
-osc = single_oscillator(alpha, omega, beta1, beta2, delta1, delta2, eps, k, dt, x_in)
+#osc = single_oscillator(alpha, omega, beta1, beta2, delta1, delta2, eps, k, dt, x_in)
+
+z = tf.Variable(tf.complex(z_init_r, z_init_i))
+x = tf.complex(x_in, 0.0)
+
+a = tf.complex(alpha, omega)
+b = tf.complex(beta1, delta1)
+d = tf.complex(beta2, delta2)
+z2 = tf.complex(tf.pow(tf.abs(z), 2), 0.0)
+z4 = tf.complex(tf.pow(tf.abs(z), 4), 0.0)
+z_ = tf.conj(z)
+passive = x * tf.reciprocal(1.0 - tf.sqrt(eps) * x)
+active = tf.reciprocal(1.0 - tf.sqrt(eps) * z_)
+
+z_step = dt * (tf.multiply(z, a + b * z2 + d * eps * z4 / (1 - eps * z2)) + k * passive * active)
+step = z.assign(z + z_step)
 
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-dt = 0.01667
-t = np.arange(0, 20, dt)
-sin = np.concatenate([0.25 * np.sin(t[:t.size//2]*2*np.pi),  np.zeros(t.size//2)])
-
 r = []
 for i in sin:
-    r.append(sess.run(osc, {x_in: i}))
+    r.append(sess.run(step, {x_in: i}))
 
+sr = np.array(r).sum(axis=1)
+plt.subplot(1,2,1)
 plt.plot(sin)
-plt.plot(r)
+plt.plot(sr)
+
+plt.subplot(1,2,2)
+ffsr = 1/sr.size * np.abs(np.fft.fft(sr)[1:sr.size//2])
+ffreq = np.fft.fftfreq(sr.size)[1:sr.size//2]
+plt.plot(ffreq, ffsr)
