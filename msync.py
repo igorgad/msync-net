09a1e01190284b4e1b1cd09164ae9436e035cc7f
igorgad <igorgad@gmail.com>
2018-10-10 13:24:23 +0200

    update

--- msync.py
+++ msync.py
@@ -1,8 +1,9 @@
 
-import dataset_interface as dts
-import models.simple_gfnn as gfnn_model
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+import dataset_interface as dts
+import models.simple_gfnn as gfnn_model
 import importlib
 importlib.reload(dts)
 importlib.reload(gfnn_model)
@@ -13,7 +14,9 @@ data_params = {'dataset_file': './data/BACH10/msync-bach10.tfrecord',
                'sample_rate': 44100//4,
                'frame_length': 2048,
                'frame_step': 2048,
-               'batch_size': 1
+               'batch_size': 1,
+               'repeat': 10,
+               'shuffle': True
                }
 
 model_params = {'num_osc': 180,
@@ -27,4 +30,18 @@ model_params = {'num_osc': 180,
 data = dts.pipeline(data_params)
 model = gfnn_model.simple_gfnn_cca_v0(model_params)
 
-model.fit(data, epochs=1, steps_per_epoch=30)
+model.fit(data, epochs=2, steps_per_epoch=10)
+
+
+# Evaluate DTW
+from dtw import dtw
+from numpy.linalg import norm
+import matplotlib.pyplot as plt
+
+r = model.predict(data, steps=1)
+r1 = r[:, :r.shape[1]//2]
+r2 = r[:, r.shape[1]//2:]
+
+dist, cost, acc_cost, path = dtw(r1.T, r2.T, dist=lambda x, y: norm(x - y, ord=1))
+plt.imshow(cost.T, origin='lower', cmap='gray', interpolation='nearest')
+plt.plot(path[0], path[1], 'w')
