6a9380bc497735ff66c21071c485fae860c36620
igorgad <igorgad@gmail.com>
2018-10-19 00:29:04 +0200

    update

--- msync_stft.py
+++ msync_stft.py
@@ -16,8 +16,8 @@ model_params = {'stft_frame_length': 512,
                 'stft_frame_step': 1,
                 'input_shape': (10240,),
                 'outdim_size': 128,
-                'pre_train_lr': 0.001,
-                'dctw_lr': 0.001,
+                'pre_train_lr': 0.0001,
+                'dctw_lr': 0.0001,
                 'v1_weights_file': './saved_models/v1_stft_weights.h5',
                 'v2_weights_file': './saved_models/v2_stft_weights.h5',
                 }
@@ -26,7 +26,7 @@ data_params = {'dataset_file': './data/BACH10/msync-bach10.tfrecord',
                'audio_root': './data/BACH10/Audio',
                'sample_rate': 44100//4,
                'example_length': 10240,
-               'batch_size': 4,
+               'batch_size': 16,
                'repeat': 10000,
                'shuffle_buffer': 32,
                'scale_value': 1.0
@@ -60,10 +60,11 @@ if not os.path.isfile(model_params['v2_weights_file']):
 
 # DCTW Training
 print('Training DCTW...')
-dctw_model.summary()
-dctw_tb = stats.TensorBoardDTW(log_dir='./logs/stft_v0/dctw0', histogram_freq=4, batch_size=data_params['batch_size'], write_images=True)
+data_params['batch_size'] = data_params['batch_size'] // 2
 dctw_data = dts.dctw_pipeline(data_params)
+dctw_tb = stats.TensorBoardDTW(log_dir='./logs/stft_v0/dctw0', histogram_freq=4, batch_size=data_params['batch_size'], write_images=True)
 
+dctw_model.summary()
 dctw_model.load_weights(model_params['v1_weights_file'], by_name=True)
 dctw_model.load_weights(model_params['v2_weights_file'], by_name=True)
 dctw_model.compile(loss=loss.cca_loss, optimizer=tf.keras.optimizers.RMSprop(lr=model_params['dctw_lr'], clipnorm=1.0))
