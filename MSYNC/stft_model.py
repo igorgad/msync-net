6657452bdc0e402df1efe332a94d04e73a625065
igorgad <igorgad@gmail.com>
2018-10-23 14:31:38 +0200

    update stft model

--- MSYNC/stft_model.py
+++ MSYNC/stft_model.py
@@ -2,12 +2,19 @@
 import tensorflow as tf
 
 
+def stft_layer_func(signal, model_params):
+    with tf.device('/device:GPU:0'):
+        stft = tf.abs(tf.contrib.signal.stft(signal, model_params['stft_frame_length'], model_params['stft_frame_step'], pad_end=True))
+        tf.summary.image('stft', tf.expand_dims(stft, -1))
+    return stft
+
+
 def simple_stft_cca(model_params):
     view1_in = tf.keras.Input(model_params['input_shape'])
     view2_in = tf.keras.Input(model_params['input_shape'])
 
-    view1_middle_out, view1_end_out = build_stft_lstm_branch(view1_in, model_params)
-    view2_middle_out, view2_end_out = build_stft_lstm_branch(view2_in, model_params)
+    view1_middle_out, view1_end_out = build_stft_dnn_branch(view1_in, model_params)
+    view2_middle_out, view2_end_out = build_stft_dnn_branch(view2_in, model_params)
     combined_output = tf.keras.layers.concatenate([view1_middle_out, view2_middle_out])
 
     view1_model = tf.keras.Model(view1_in, view1_end_out)
@@ -40,8 +47,19 @@ def build_stft_lstm_branch(input, model_params):
     return middle_output, end_output
 
 
-def stft_layer_func(signal, model_params):
-    with tf.device('/device:GPU:0'):
-        stft = tf.abs(tf.contrib.signal.stft(signal, model_params['stft_frame_length'], model_params['stft_frame_step'], pad_end=True))
-        tf.summary.image('stft', tf.expand_dims(stft, -1))
-    return stft
+def build_stft_dnn_branch(input, model_params):
+    stft = tf.keras.layers.Lambda(lambda signal: stft_layer_func(signal, model_params))(input)
+
+    middle_output = tf.keras.layers.BatchNormalization()(stft)
+    middle_output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(2 * model_params['outdim_size'], activation='sigmoid'))(middle_output)
+    middle_output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(2 * model_params['outdim_size'], activation='sigmoid'))(middle_output)
+    middle_output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(2 * model_params['outdim_size'], activation='sigmoid'))(middle_output)
+    middle_output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(model_params['outdim_size'], activation='linear'))(middle_output)
+
+    end_output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(model_params['outdim_size'], activation='linear'))(middle_output)
+    end_output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(2 * model_params['outdim_size'], activation='sigmoid'))(end_output)
+    end_output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(2 * model_params['outdim_size'], activation='sigmoid'))(end_output)
+    end_output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(2 * model_params['outdim_size'], activation='sigmoid'))(end_output)
+    end_output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(1, activation='linear'))(end_output)
+    end_output = tf.keras.layers.Flatten()(end_output)
+    return middle_output, end_output
