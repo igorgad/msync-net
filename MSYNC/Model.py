e03c34a85bd01a6b055e92a8282ece7aec98d399
Igor <igorgad@dca.ufrn.br>
2018-12-09 21:53:33 +0100

    update

--- MSYNC/Model.py
+++ MSYNC/Model.py
@@ -19,7 +19,8 @@ class MSYNCModel:
         output = tf.keras.layers.TimeDistributed(tf.keras.layers.GlobalAveragePooling2D(), name=name + 'GAverage')(encoded)
         output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(256), name=name + 'fc_block1/fc')(output)
         output = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization(), name=name + 'fc_block1/bn')(output)
-        output = tf.keras.layers.TimeDistributed(tf.keras.layers.LeakyReLU(), name=name + 'fc_block1/elu')(output)
+        output = tf.keras.layers.TimeDistributed(tf.keras.layers.LeakyReLU(), name=name + 'fc_block1/lrelu')(output)
+        output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dropout(self.dropout_rate), name=name + 'fc_block1/dropout')(output)
         output = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(128), name=name + 'fc_block2/fc')(output)
         output = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization(), name=name + 'fc_block2/bn')(output)
 
--- msync.py
+++ msync.py
@@ -25,7 +25,7 @@ data_params = {'sample_rate': 16000,
                'instrument_2': 'clarinet' if dataset == 'bach10' else 'clean electric guitar'
                }
 
-logname = 'with_ae-vgg-2fc-' + dataset + ''.join(['-%s=%s' % (key, value) for (key, value) in train_params.items()])
+logname = 'with_ae-vgg-2fc-drop=0.5-' + dataset + ''.join(['-%s=%s' % (key, value) for (key, value) in train_params.items()])
 logname = logname + ''.join(['-%s=%s' % (key, str(value).replace(' ', '_')) for (key, value) in data_params.items()])
 print (logname)
 
