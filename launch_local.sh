ee9f98b1a1d1fd44a5171f2f8def68615724db80
Igor <igorgad@dca.ufrn.br>
2018-12-13 15:45:10 +0100

    update

--- launch_local.sh
+++ launch_local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-DATASET_FILE=~/workspace/Dataset/BACH10/MSYNC-bach10.tfrecord
-DATASET_AUDIO_DIR=~/workspace/Dataset/BACH10/Audio/
+DATASET_FILE=./data/BACH10/MSYNC-bach10.tfrecord
+DATASET_AUDIO_DIR=./data/BACH10/Audio/
 LOG_DIR=./logs/
 EPOCHS=50
 
